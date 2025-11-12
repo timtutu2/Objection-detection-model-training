c95df05702e6ae97c522be027604b25daf23df65
timtutu2 <timtutu2@users.noreply.github.com>
2025-11-12 15:35:29 -0800

    preprocessing.sh

--- preprocessing.sh
+++ preprocessing.sh
@@ -1,18 +1,13 @@
 apt-get update
-apt-get install wget unzip -y
+apt-get install wget unzip python3-pip -y
+pip3 install gdown
 
-if [ -d "vis-data-256" ] || [ -f "vis-data-256.zip" ]; then
-    echo "vis-data-256 directory or zip file already exists"
-elif [ -d "vis-data-256-processed" ]; then
+if [ -d "Car_data-256" ] || [ -f "Car_data-256.zip" ]; then
+    echo "Car_data-256 directory or zip file already exists"
+elif [ -d "Car_data-256-processed" ]; then
     echo "Processed dataset already exists, skipping raw dataset download and extraction"
 else
-    wget https://web.eecs.umich.edu/~ahowens/vis/vis-data-256.zip --no-check-certificate
-    unzip vis-data-256.zip
-    rm vis-data-256.zip
-fi
-
-if [ -d "vis-data-256-processed" ]; then
-    echo "Dataset is already cached, skipping dataset processing"
-else
-    python process_greatest_hits.py
+    gdown https://drive.google.com/uc?id=1qL-1PV1jvNDRF_yToKUGptVpvmZUfor1 -O Car_data-256.zip
+    unzip Car_data-256.zip
+    rm Car_data-256.zip
 fi
