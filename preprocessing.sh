f1f2be4dd7597d574a80204d133c70658cf603e7
timtutu2 <timtutu2@users.noreply.github.com>
2025-11-13 13:58:42 -0800

    test

--- preprocessing.sh
+++ preprocessing.sh
@@ -4,10 +4,16 @@ pip3 install gdown
 
 if [ -d "Car_data-256" ] || [ -f "Car_data-256.zip" ]; then
     echo "Car_data-256 directory or zip file already exists"
-elif [ -d "Car_data-256-processed" ]; then
-    echo "Processed dataset already exists, skipping raw dataset download and extraction"
 else
     gdown https://drive.google.com/uc?id=1qL-1PV1jvNDRF_yToKUGptVpvmZUfor1 -O Car_data-256.zip
     unzip Car_data-256.zip
     rm Car_data-256.zip
+fi
+
+if [ -d "Car_data_test-256" ] || [ -f "Car_data-256_test.zip" ]; then
+    echo "Car_data_test-256 directory or zip file already exists"
+else
+    gdown https://drive.google.com/uc?id=1E5mqA18Dto2l0MjqERjQm3BSAPbcwoeF -O Car_data_test-256.zip
+    unzip Car_data_test-256.zip
+    rm Car_data_test-256.zip
 fi
