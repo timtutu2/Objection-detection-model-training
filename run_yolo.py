e5e077dae3e03677977d334e6d612f1a48697f6f
timtutu2 <timtutu2@users.noreply.github.com>
2025-11-12 16:01:03 -0800

    change path

--- run_yolo.py
+++ run_yolo.py
@@ -2,20 +2,20 @@ import sys
 from pathlib import Path
 
 # add yolov5 project to Python path
-sys.path.append("/workspace/yolov5")
+sys.path.append("/pers_vol/yolov5-nrp/yolov5")
 
 from train import run  # this time is import yolov5/train.py in run
 
 if __name__ == "__main__":
     run(
-        data="/workspace/model_training/Car_model.yaml",
+        data="/pers_vol/yolov5-nrp/Car_model.yaml",
         weights="yolov5s.pt",
         imgsz=640,
         epochs=50,
         batch_size=16,
         device="0",
-        project="runs/train",
-        name="car_house_yolov5s",
+        project="/pers_vol/yolov5-nrp/runs_obj_det/train",
+        name="car_yolov5s",
         exist_ok=True,
         workers=2,
     )
