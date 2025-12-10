49ae55b8c8617c27399496f52f3323fd776bf1cd
timtutu2 <timtutu2@users.noreply.github.com>
2025-12-10 01:12:59 -0800

    clahe 30

--- test/test_253_local.py
+++ test/test_253_local.py
@@ -59,7 +59,7 @@ if __name__ == "__main__":
     # config
     weights_path = "/workspace/datasets/model_training/yolo5_best.pt"
     data_yaml = "/workspace/datasets/model_training/test/test_253_dataset.yaml"
-    output_dir = "/workspace/datasets/model_training/test_dataset_overexposure/original"
+    output_dir = "/workspace/datasets/model_training/test_dataset_overexposure/CLANE/enh_bef/Q30"
     
     # test parameters
     batch_size = 16        
